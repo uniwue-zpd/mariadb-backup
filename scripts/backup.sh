e39bf7bf2389536652df13ba2637996709e7545d
maxnth <maximilian.noeth@protonmail.com>
2024-04-02 15:03:38 +0200

    add basic error logging

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 DATE=$(date +%F)
 mkdir -p /backup/${DATE}
-mariadb-dump --user=$DB_USER --password=$DB_PASSWORD --lock-tables --databases $DB_NAME > /backup/${DATE}/dump.sql
+echo "[$(date +%T)] Dumping database ${DB_NAME} into /backup/${DATE}/dump.sql" >> /mysql_backup.log
+mariadb-dump --user=$DB_USER --password=$DB_PASSWORD --lock-tables --databases $DB_NAME --log-errors=/mysql_backup.log > /backup/${DATE}/dump.sql
+echo "[$(date +%T)] Database dump finished" >> /mysql_backup.log
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,5 @@ touch /mysql_backup.log
 echo "${CRON_INTERVAL} sh /scripts/backup.sh" > /etc/cron.d/smw-cron
 chmod 0644 /etc/cron.d/smw-cron
 crontab /etc/cron.d/smw-cron
-touch /var/log/cron.log
 
 cron && tail -f /mysql_backup.log
