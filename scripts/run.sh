87925e3642bafdc9650c8e2c993a13b7beb58494
maxnth <maximilian.noeth@protonmail.com>
2024-12-11 14:38:07 +0100

    wrap env values in quotes

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,6 @@ chmod 0644 /etc/cron.d/smw-cron
 crontab /etc/cron.d/smw-cron
 
 printenv | grep -v "no_proxy" > /etc/environment
-printenv | grep -v "no_proxy" | sed 's/^/export /' > /etc/cron.env
+printenv | grep -v "no_proxy" | sed 's/^/export /; s/=/="/' | sed 's/$/"/') > /etc/cron.env
 
 cron && tail -f /mysql_backup.log
